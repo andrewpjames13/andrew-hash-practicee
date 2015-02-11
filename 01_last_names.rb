c8458e6bc919d15e58577f78d3c815847733087e
Andrew James <andrewpjames87@gmail.com>
2015-02-11 09:31:56 -0700

    Puts all peoples last_name

--- 01_last_names.rb
+++ 01_last_names.rb
@@ -1,3 +1,7 @@
 require_relative 'people'
 
-# What are the last names of all of the people?
+# What are the last ames of all of the people?
+
+
+puts PEOPLE["joe"][:last_name]
+puts PEOPLE["sue"][:last_name]
